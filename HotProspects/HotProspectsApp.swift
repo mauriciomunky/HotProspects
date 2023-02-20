2a1b6e8316e02a9008a6faa87bf9067e4071cb89
Maurício Costa <86126994+mauriciomunky@users.noreply.github.com>
2023-02-20 13:22:10 -0300

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Maurício Costa on 20/02/23.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
